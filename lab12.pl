a6861181db367d8be3bda66ef6aba26d441d7a24
xLagerFeuer <alexander.zhdanoff@gmail.com>
2022-04-22 16:17:18 +0300

    initial commit

---
+++ lab12.pl
@@ -0,0 +1,41 @@
+% Variant 3. Zhdanov Alexander
+
+
+% 11
+% max prime divider x
+
+
+% 12
+% gcd max odd nonprime divider x && digits product x
+
+
+% 13
+% d < 1000: 1/d самый большой период w/o lists
+% answer: 1/3 = 0,(3)
+
+
+
+% 14
+% predicat: return list length
+
+
+% 15Ч20
+% Variant 3 Ч 3,11,13,15,27,30
+
+% 15.3
+% max
+
+% 16.11
+% between
+
+% 17.13
+%
+
+% 18.15
+% max
+
+% 19.27
+% max
+
+% 20.30
+% max
